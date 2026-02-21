efb198a9ca683610f602864576fefb3f3860888f
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-02-22 02:55:24 +0300

    Small clearing edit

--- Transact-SQL/SQLQueryHW - INSERT 12-21.sql
+++ Transact-SQL/SQLQueryHW - INSERT 12-21.sql
@@ -17,28 +17,27 @@ DECLARE @number_of_lessons_1	AS	TINYINT =(SELECT number_of_lessons	FROM Discipli
 DECLARE @discipline_2			AS	INT		=(SELECT discipline_id		FROM Disciplines	WHERE discipline_name LIKE N'%Hardware%');
 DECLARE @number_of_lessons_2	AS	TINYINT =(SELECT number_of_lessons	FROM Disciplines	WHERE discipline_id=@discipline_2);
 DECLARE @start_time				AS	TIME	=(SELECT start_time			FROM Groups			WHERE group_id=@group);
------------------------------------------------------------------------------------------------------------------------------------------
-PRINT(@group);
-PRINT(@discipline_1);
-PRINT(@discipline_2);
-PRINT(@number_of_lessons_1);
-PRINT(@number_of_lessons_2);
-PRINT(@teacher_1);
-PRINT(@start_date_1);
-PRINT(@start_date_2);
-PRINT(@start_time);
+--> PRINT BASE <-------------------------------------------------------------------------------------------------------------------------
+--PRINT(@group);
+--PRINT(@discipline_1);
+--PRINT(@discipline_2);
+--PRINT(@number_of_lessons_1);
+--PRINT(@number_of_lessons_2);
+--PRINT(@start_date_1);
+--PRINT(@start_date_2);
+--PRINT(@start_time);
 -----------------------------------------------------------------------------------------------------------------------------------------
 DECLARE @date				AS DATE		= IIF(@start_date_1 < @start_date_2, @start_date_1 , @start_date_2);
-PRINT(DATEPART(WEEKDAY, @date));
-SET @date	=	DATEADD(DAY
-,CASE DATEPART(WEEKDAY, @date) WHEN 2 THEN 1 WHEN 4 THEN 1 WHEN 7 THEN 1 WHEN 6 THEN 2 ELSE 0 END, @date)
-PRINT(DATEPART(WEEKDAY, @date));
+--PRINT(DATEPART(WEEKDAY, @date));
+SET @date	=	DATEADD(DAY,CASE DATEPART(WEEKDAY, @date) WHEN 2 THEN 1 WHEN 4 THEN 1 WHEN 7 THEN 1 WHEN 6 THEN 2 ELSE 0 END, @date)--WeekDayFilter
+--PRINT(DATEPART(WEEKDAY, @date));
 DECLARE @lesson_number_1	AS TINYINT	= @number_of_lessons_1;
 DECLARE @lesson_number_2	AS TINYINT	= @number_of_lessons_2;
 DECLARE @wednesday_switch	AS BIT		= IIF(@date=@start_date_1,1,0);
 DECLARE @name_discipline	AS NCHAR(10);
 DECLARE @time	AS TIME(0) = @start_time;
 PRINT(FORMATMESSAGE(N'%s %s %s %s, %s',N'Старт с ',CAST(DATEPART(WEEKDAY, @date) AS VARCHAR(24)),N'дня недели: ', DATENAME(WEEKDAY,@date), CAST(@date AS VARCHAR(24))))
+PRINT(N'---------------------------------------------------------')
 -----------------------------------------------------------------------------------------------------------------------------------------
 WHILE	(@lesson_number_1 > 0 OR @lesson_number_2 > 0)
 	BEGIN
@@ -48,7 +47,7 @@ WHILE	(@lesson_number_1 > 0 OR @lesson_number_2 > 0)
 -------> HardwarePC
 		IF(@day = 1)
 			BEGIN
-				IF @lesson_number_2 > 0 AND @start_date_2 <= @date
+				IF			@lesson_number_2 > 0 AND @start_date_2 <= @date
 					BEGIN
 						SET @lesson = @lesson_number_2
 						SET @name_discipline = @name_discipline_2
@@ -67,14 +66,14 @@ WHILE	(@lesson_number_1 > 0 OR @lesson_number_2 > 0)
 				--AND 
 				IF @wednesday_switch = 0 
 					BEGIN
-						IF @lesson_number_2 > 0 AND @start_date_2 <= @date
+						IF			@lesson_number_2 > 0 AND @start_date_2 <= @date
 							BEGIN
 								SET @lesson = @lesson_number_2
 								SET @name_discipline = @name_discipline_2
 								SET @lesson_number_2 = @lesson_number_2 - 1
 								SET @wednesday_switch = IIF(@wednesday_switch=0,1,0)		
 							END
-						ELSE IF @lesson_number_1 > 0 AND @start_date_1 <= @date
+						ELSE IF		@lesson_number_1 > 0 AND @start_date_1 <= @date
 							BEGIN
 								SET @lesson = @lesson_number_1
 								SET @name_discipline = @name_discipline_1
@@ -83,16 +82,15 @@ WHILE	(@lesson_number_1 > 0 OR @lesson_number_2 > 0)
 							END
 					END
 				ELSE IF @wednesday_switch = 1 
-				--AND @lesson_number_1 > 0 AND @start_date_1 <= @date
 					BEGIN
-						IF @lesson_number_1 > 0 AND @start_date_1 <= @date
+						IF			@lesson_number_1 > 0 AND @start_date_1 <= @date
 							BEGIN
 								SET @lesson = @lesson_number_1
 								SET @name_discipline = @name_discipline_1
 								SET @lesson_number_1 = @lesson_number_1 - 1
 								SET @wednesday_switch = IIF(@wednesday_switch=0,1,0)
 							END
-						ELSE IF @lesson_number_2 > 0 AND @start_date_2 <= @date
+						ELSE IF		@lesson_number_2 > 0 AND @start_date_2 <= @date
 							BEGIN
 								SET @lesson = @lesson_number_2
 								SET @name_discipline = @name_discipline_2
@@ -101,11 +99,10 @@ WHILE	(@lesson_number_1 > 0 OR @lesson_number_2 > 0)
 							END
 					END
 			END
-			--IIF(@lesson_number_1 > 0, SET @lesson_number_1 = @lesson_number_1 - 1 AND SET @lesson = @lesson_number_1, SET @lesson_number_2 = @lesson_number_2 - 1 AND SET @lesson = @lesson_number_2);
 -------> ДЕНЬ С++
 		IF(@day = 5)
 			BEGIN
-				IF @lesson_number_1 > 0 AND @start_date_1 <= @date
+				IF		@lesson_number_1 > 0 AND @start_date_1 <= @date
 					BEGIN
 						SET @lesson = @lesson_number_1
 						SET @name_discipline = @name_discipline_1
@@ -113,12 +110,9 @@ WHILE	(@lesson_number_1 > 0 OR @lesson_number_2 > 0)
 					END
 				ELSE IF @lesson_number_2 > 0 AND @start_date_2 <= @date
 					BEGIN
-						--
-						--	BEGIN
-								SET @lesson = @lesson_number_2;
-								SET @name_discipline = @name_discipline_2
-								SET @lesson_number_2 = @lesson_number_2 - 1
-							--END
+						SET @lesson = @lesson_number_2;
+						SET @name_discipline = @name_discipline_2
+						SET @lesson_number_2 = @lesson_number_2 - 1
 					END
 			END
 --> Lesson_1
@@ -140,12 +134,13 @@ WHILE	(@lesson_number_1 > 0 OR @lesson_number_2 > 0)
 		, CAST(@date AS VARCHAR(24))
 		, DATENAME(WEEKDAY, @date)
 		,IIF(@day=3,N'',N'   ')
-		, CAST(@time AS VARCHAR(24)),@name_discipline
+		, CAST(@time AS VARCHAR(24))
+		,@name_discipline
 		, CAST(IIF(@name_discipline = @name_discipline_1,N'Олег', N'Свищев' ) AS VARCHAR(24))));
 ------->
 		--IF NOT EXISTS (SELECT lesson_id FROM Schedule WHERE [date]=@date AND [time]=@time AND [group]=@group)
 		--INSERT Schedule VALUES (@group, @discipline, @teacher, @date, @time, IIF(@date<GETDATE(),1,0));
 -------<
 		SET @date	=	DATEADD(DAY,IIF(@day = 5,3,2),@date);
-		IF(@day = 5) PRINT(N'------------------')
+		IF(@day = 5) PRINT(N'---------------------------------------------------------')
 	END
