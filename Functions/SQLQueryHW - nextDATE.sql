994c07cdd180f68898cc0f08ebe8a6d32117629f
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-03-02 15:51:35 +0300

    Work, not OK

--- Functions/SQLQueryHW - nextDATE.sql
+++ Functions/SQLQueryHW - nextDATE.sql
@@ -1,4 +1,23 @@
 -- SQLQueryHW - nextDATE.sql
 
 USE PV_521_Import;
+SET DATEFIRST 1;
+GO
 
+CREATE OR ALTER FUNCTION NextDate(@prevDate AS DATE)RETURNS DATE
+AS
+BEGIN
+	SET		@prevDate	=	DATEADD(DAY,CASE DATEPART(WEEKDAY, @prevDate) WHEN 2 THEN 1 WHEN 4 THEN 1 WHEN 7 THEN 1 WHEN 6 THEN 2 ELSE 0 END, @prevDate)--WeekDayFilter
+		IF @prevDate = (SELECT date FROM DaysOFF WHERE @prevDate = date)
+			WHILE @prevDate = (SELECT date FROM DaysOFF WHERE @prevDate = date)
+				BEGIN
+					DECLARE @day	AS TINYINT		=	DATEPART(WEEKDAY, @prevDate);
+					SET @prevDate	=	DATEADD(DAY,IIF(@day = 5,3,2),@prevDate);
+				END	
+		ELSE
+			BEGIN
+				DECLARE @day2	AS TINYINT		=	DATEPART(WEEKDAY, @prevDate);
+				SET @prevDate	=	DATEADD(DAY,IIF(@day2 = 5,3,2),@prevDate);
+			END
+	RETURN @prevDate ;
+END
