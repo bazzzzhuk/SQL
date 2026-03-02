9b3df37a8cfcfd789abfa9680d89d3016a85621a
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-03-02 21:27:00 +0300

    GetNextLearnDay Done, Need Check

---
+++ Functions/SQLQuery1 - Check.sql
@@ -0,0 +1,8 @@
+-- SQLQuery1 - Check.sql
+
+USE PV_521_Import;
+SET DATEFIRST 1;
+GO
+
+
+PRINT dbo.GetNextLearnDay(N'PV_521')
--- StoredProcedures/SQLQuery - CHECK.sql
+++ StoredProcedures/SQLQuery - CHECK.sql
@@ -10,5 +10,5 @@ SET DATEFIRST 1;
 --DELETE FROM Schedule WHERE discipline=(SELECT discipline_id FROM Disciplines	WHERE discipline_name LIKE N'%Теория баз данных, программирование MS SQL Server%')
 
 EXEC sp_InsertScheduleStacionar N'PV_521', N'%ADO.NET%', N'Олег', N'2025-01-01';
-EXEC sp_InsertScheduleStacionar N'PV_521', N'%Объектно%', N'Олег', N'2025-02-10';
+EXEC sp_InsertScheduleStacionar N'PV_521', N'%Объектно%', N'Олег', N'2025-02-13';
 EXEC sp_SelectScheduleFor		N'PV_521';
