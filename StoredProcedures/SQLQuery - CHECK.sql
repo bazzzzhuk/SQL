7022b614d972f9c3c856e879959d4cd5c905eeb6
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-03-15 22:37:21 +0300

    1221 HW Done

--- StoredProcedures/SQLQuery - CHECK.sql
+++ StoredProcedures/SQLQuery - CHECK.sql
@@ -11,13 +11,13 @@ SET DATEFIRST 1;
 --DELETE FROM Schedule WHERE discipline=(SELECT discipline_id FROM Disciplines	WHERE discipline_name LIKE N'%Hardware-PC%')
 --DELETE FROM Schedule WHERE discipline=(SELECT discipline_id FROM Disciplines	WHERE discipline_name LIKE N'%Теория баз данных, программирование MS SQL Server%')
 
---DELETE FROM Schedule WHERE [group] = 521 --AND [date]>=N'2026-03-07'
-EXEC sp_InsertSchedule1221 N'PV_521', N'Hardware%', N'Свищев',N'2025-01-20',1,3,5;
-DECLARE @bb AS BIT;
-IF EXISTS (SELECT lesson_id FROM Schedule WHERE [date] = N'2025-01-22' AND discipline = 31) SET @bb = 1 ELSE SET @bb = 0;
-PRINT @bb;
-EXEC sp_InsertSchedule1221 N'PV_521', N'%Windows', N'Свищев',N'2025-04-30',1,3,5;
-EXEC sp_InsertSchedule1221 N'PV_521', N'Процедурное%C++', N'Ковтун',N'2025-01-20',5,3,1;
+DELETE FROM Schedule WHERE [group] = 521 --AND [date]>=N'2026-03-07'
+--EXEC sp_InsertSchedule1221 N'PV_521', N'Hardware%', N'Свищев',N'2025-01-20',1,3,5;
+--DECLARE @bb AS BIT;
+--IF EXISTS (SELECT lesson_id FROM Schedule WHERE [date] = N'2025-01-22' AND discipline = 31) SET @bb = 1 ELSE SET @bb = 0;
+--PRINT @bb;
+--EXEC sp_InsertSchedule1221 N'PV_521', N'%Windows', N'Свищев',N'2025-04-30',1,3,5;
+--EXEC sp_InsertSchedule1221 N'PV_521', N'Процедурное%C++', N'Ковтун',N'2025-01-20',5,3,1;
 --UPDATE Schedule SET teacher = 1 WHERE discipline =1;
 
 --EXEC sp_InsertScheduleStacionar N'PV_521', N'%Объектно%', N'Олег', N'2025-02-13';
@@ -30,10 +30,13 @@ EXEC sp_InsertSchedule1221 N'PV_521', N'Процедурное%C++', N'Ковт�
 --EXEC sp_InsertScheduleStacionar N'PV_521', N'ReactJS', N'Олег', DEFAULT;
 --EXEC sp_InsertScheduleStacionar N'PV_521', N'%ASP.NET', N'Олег', DEFAULT;
 --EXEC sp_InsertScheduleStacionar N'PV_521', N'%ADO.NET%', N'Олег', N'2026-01-21';
-EXEC sp_SelectScheduleFor		N'PV_521';
  
 --EXEC sp_InsertScheduleStacionar N'PV_319', N'%ADO.NET%', N'Олег', N'2027-01-01';
 --EXEC sp_InsertScheduleStacionar N'Java_326', N'%ADO.NET%', N'Олег', N'2024-12-20';
 --EXEC sp_InsertScheduleStacionar N'PV_319', N'%Объектно%', N'Олег', N'2025-02-13';
 --EXEC sp_SelectScheduleFor		N'PV_319';
 --EXEC sp_SelectScheduleFor		N'Java_326';
+
+EXEC sp_1221_InsertSchedule N'PV_521', N'%ADO.NET%', N'Олег', N'2025-02-03', N'%Hardware-PC%', N'Свищев', N'2025-02-14';
+
+EXEC sp_SelectScheduleFor		N'PV_521';
--- StoredProcedures/SQLQueryHW - sp_1221_InsertSchedule.sql
+++ StoredProcedures/SQLQueryHW - sp_1221_InsertSchedule.sql
@@ -5,12 +5,12 @@ GO
 
 CREATE OR ALTER PROCEDURE sp_1221_InsertSchedule  
 	 @group_name					AS NCHAR(10)
-	,@discipline_name_1				AS NVARCHAR(150)
 	,@discipline_name_2				AS NVARCHAR(150)
-	,@teacher_name_1				AS NVARCHAR(50)
 	,@teacher_name_2				AS NVARCHAR(50)
-	,@start_date_1					AS DATE
 	,@start_date_2					AS DATE
+	,@discipline_name_1				AS NVARCHAR(150)
+	,@teacher_name_1				AS NVARCHAR(50)
+	,@start_date_1					AS DATE
 AS
 BEGIN
 	DECLARE @group					AS INT			 =	(SELECT group_id			FROM Groups		 WHERE group_name	    =    @group_name);
@@ -24,45 +24,50 @@ BEGIN
 
 	DECLARE @date					AS DATE				= IIF(@start_date_1 < @start_date_2, @start_date_1 , @start_date_2);
 	DECLARE @teacher				AS SMALLINT;
-	DECLARE @teacher_name			AS NVARCHAR(50);
 	DECLARE @discipline				AS SMALLINT;
-	DECLARE @name_discipline		AS NVARCHAR(150);
 	DECLARE	@lesson_number			AS TINYINT;
-	DECLARE @lesson_number_1		AS TINYINT		 = @number_of_lessons_1;
-	DECLARE @lesson_number_2		AS TINYINT		 = @number_of_lessons_2;
+	DECLARE @lesson_number_1		AS TINYINT		 = 0;
+	DECLARE @lesson_number_2		AS TINYINT		 = 0;
 	DECLARE @time					AS TIME(0)		 = @start_time;
 	DECLARE @wednesday_switch		AS BIT			 = IIF(@date=@start_date_1,1,0);
-
+--->
 	WHILE	(@lesson_number_1 < @number_of_lessons_1 OR @lesson_number_2 < @number_of_lessons_2) -- OR!!!
 	BEGIN
-		DECLARE @day			AS TINYINT	=	DATEPART(WEEKDAY, @date);
+		DECLARE @day				AS TINYINT		 = DATEPART(WEEKDAY, @date);
 		SET		@time = @start_time;
--------> День HardwarePC				(Понедельник)
-		IF(@day = 1) IF @lesson_number_2 > 0 AND @start_date_2 <= @date SET @discipline = @discipline_2 ELSE SET @discipline = @discipline_1
--------> ДЕНЬ С++/HardwarePC	(Среда)
-		IF(@day = 3)																	BEGIN
-				IF @wednesday_switch = 0												BEGIN
-						IF			@lesson_number_2 > 0 AND @start_date_2 <= @date		BEGIN
-								SET @discipline = @discipline_2
-								SET @wednesday_switch = IIF(@wednesday_switch=0,1,0)	END
-						ELSE IF		@lesson_number_1 > 0 AND @start_date_1 <= @date		BEGIN
-								SET @discipline = @discipline_1
-								SET @wednesday_switch = IIF(@wednesday_switch=0,1,0)	END END
-				ELSE IF @wednesday_switch = 1											BEGIN
-						IF			@lesson_number_1 > 0 AND @start_date_1 <= @date		BEGIN
-								SET @discipline = @discipline_1
-								SET @wednesday_switch = IIF(@wednesday_switch=0,1,0)	END
-						ELSE IF		@lesson_number_2 > 0 AND @start_date_2 <= @date		BEGIN
-								SET @discipline = @discipline_2
-								SET @wednesday_switch = IIF(@wednesday_switch=0,1,0)	END END END
--------> ДЕНЬ С++				(Пятница)
-		IF(@day = 5) IF	@lesson_number_1 > 0 AND @start_date_1 <= @date SET @discipline = @discipline_1 ELSE IF	@lesson_number_2 > 0 AND @start_date_2 <= @date SET @discipline = @discipline_2
-------->
-		SET			@teacher		= IIF(@discipline = @discipline_1, @teacher_1, @teacher_2);
-		SET 		@lesson_number  = IIF(@discipline = @discipline_1, @lesson_number_1, @lesson_number_2);
+		---> Понедельник
+		IF(@day = 1) IF @lesson_number_2 < @number_of_lessons_2 AND @start_date_2 <= @date SET @discipline = @discipline_2 ELSE SET @discipline = @discipline_1
+		---> Среда
+		IF(@day = 3)																BEGIN
+			IF		@wednesday_switch = 0											BEGIN
+					IF			@lesson_number_2 < @number_of_lessons_2 
+							AND @start_date_2 <= @date								BEGIN
+							SET @discipline = @discipline_2
+							SET @wednesday_switch = IIF(@wednesday_switch=0,1,0)	END
+					ELSE IF		@lesson_number_1 < @number_of_lessons_1 
+							AND @start_date_1 <= @date								BEGIN
+							SET @discipline = @discipline_1
+							SET @wednesday_switch = IIF(@wednesday_switch=0,1,0)	END END
+			ELSE IF @wednesday_switch = 1											BEGIN
+					IF			@lesson_number_1 < @number_of_lessons_1 
+								AND @start_date_1 <= @date							BEGIN
+							SET @discipline = @discipline_1
+							SET @wednesday_switch = IIF(@wednesday_switch=0,1,0)	END
+					ELSE IF		@lesson_number_2 < @number_of_lessons_2 
+							AND @start_date_2 <= @date								BEGIN
+							SET @discipline = @discipline_2
+							SET @wednesday_switch = IIF(@wednesday_switch=0,1,0)	END END END
+		---> Пятница
+		IF(@day = 5) IF	@lesson_number_1 < @number_of_lessons_1 AND @start_date_1 <= @date SET @discipline = @discipline_1 ELSE IF	@lesson_number_2 < @number_of_lessons_2 AND @start_date_2 <= @date SET @discipline = @discipline_2
+--->
+		SET		@teacher		= IIF(@discipline = @discipline_1, @teacher_1, @teacher_2);
+		SET		@lesson_number  = IIF(@discipline = @discipline_1, @lesson_number_1, @lesson_number_2);
 
 		EXEC	sp_InsertLesson @group, @discipline, @teacher, @date, @time OUTPUT, @lesson_number OUTPUT;
 		EXEC	sp_InsertLesson @group, @discipline, @teacher, @date, @time OUTPUT, @lesson_number OUTPUT;
+
+		IF @discipline = @discipline_1 SET @lesson_number_1 = @lesson_number ELSE SET @lesson_number_2 = @lesson_number;
+		SET		@date = dbo.GetNextLearnDate(@group_name, @date);
 	END
 
 END
